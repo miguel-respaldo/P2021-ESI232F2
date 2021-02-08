15da5db5e31b04982b98d10128fbddbd7f6f8dfc
Miguel Bernal Marin <miguel.bernal@iteso.mx>
2021-02-08 12:26:53 -0600

    mas cadenas

--- 0208 - Entadas y Salidas/concatenacion_de_cadenas.py
+++ 0208 - Entadas y Salidas/concatenacion_de_cadenas.py
@@ -10,3 +10,22 @@ print(s)
 s3 = 3*s1  # Repetición 3 veces
 
 print(s3)
+
+# Algoritmos para imprimir la s3 con espacios
+
+s3 = 3*(s1+" ")
+print(s3)
+
+# Imprimir solo una parte de la cadena
+# De Algoritmos imprimir solo Algo
+#    0123456789
+print(s1[0:4]) # Subcadena del 0 y menor que 4
+# Subcadnea  s[inicio:fin] donde es desde inicio < fin
+#   inicio <= x < fin
+
+print(s1[4:9]) #imprimir ritmo
+
+## s1 es "Algoritmos"
+print("algo" in s1)
+print("gramo" in s2)
+
