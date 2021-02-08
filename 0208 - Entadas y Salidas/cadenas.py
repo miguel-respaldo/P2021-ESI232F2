23dd7a54dd33fde4c695d97d3e75b07eba3e32a5
Miguel Bernal Marin <miguel.bernal@iteso.mx>
2021-02-08 12:01:16 -0600

    comenzamos con cadenas

---
+++ 0208 - Entadas y Salidas/cadenas.py
@@ -0,0 +1,19 @@
+# Inicializar una cadena
+
+s1 = str() # Esta es una cadena vacia
+s2 = ""    # Tambien es una cadena vacia
+
+s3 = str("un texto")  # inicializamos con un texto
+s4 = "un texto"
+
+print(s3)  # imprime "un texto" sin las comillas
+print(s4)  # imprime "un texto" sin las comillas
+
+print(s3[0]) # imprime "u"
+
+# "un texto"
+#  01234567
+
+print(s3[5]) # imprime "x"
+print(s3[7]) # imprime "o"
+
