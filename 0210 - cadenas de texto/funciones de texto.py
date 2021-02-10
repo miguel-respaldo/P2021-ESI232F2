bfa12f8b149cbb724e8c8c3f2fce7f8c5d2c337d
Miguel Bernal Marin <miguel.bernal@iteso.mx>
2021-02-10 11:48:14 -0600

    buscando cadenas

--- 0210 - cadenas de texto/funciones de texto.py
+++ 0210 - cadenas de texto/funciones de texto.py
@@ -3,5 +3,14 @@
 s = "Hola Mundo"
 print("isdigit:", s.isdigit())
 print("isalpha:", s.isalpha())
-print("isupper:", s.isupper())
-print("islower:", s.islower())
+print("isupper:", s.isupper())  # Upper Case
+print("islower:", s.islower())  # Lower Case
+
+### Buscando sub candenas
+
+s = 'Bienvenido a Python'
+print("termina con thon: ", s.endswith("thon"))
+print("comienza con Buen", s.startswith("Buen"))
+print("Posición de ido: ", s.find("ido"))
+print("Posición derecha de venida:", s.rfind("venida"))
+print("Número de o:", s.count("o"))
