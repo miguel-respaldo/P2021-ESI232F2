7bd7ef713fc227c58e06b26d89d6ff19c011b28f
Miguel Bernal Marin <miguel.bernal@iteso.mx>
2021-02-22 11:22:11 -0600

    potencias y raices 2

--- 0222 - sentencias selectivas/01_potencias_y_raices.py
+++ 0222 - sentencias selectivas/01_potencias_y_raices.py
@@ -5,6 +5,8 @@ numero = int(input("Ingresa un número: "))
 
 raiz = math.sqrt(numero)
 cuadrado = numero**2
+cuadrado2 = math.pow(numero,2)
 
 print("La raiz del numero es", raiz)
 print("El cuadrado del numero es", cuadrado)
+print("El cuadrado2 del numero es", cuadrado2)
