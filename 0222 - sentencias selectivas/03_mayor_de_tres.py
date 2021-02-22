164962cabca26d0883f0743ddf031b13a9269738
Miguel Bernal Marin <miguel.bernal@iteso.mx>
2021-02-22 12:10:54 -0600

    el mayor de 3 en python

--- 0222 - sentencias selectivas/03_mayor_de_tres.py
+++ 0222 - sentencias selectivas/03_mayor_de_tres.py
@@ -4,3 +4,17 @@
 #  2. Comparar 2 primero y luego otros 2
 #  3. Al final digo cual es el mayor
 
+num1 = int(input("Escribe el primer número: "))
+num2 = int(input("Escribe el segundo número: "))
+num3 = int(input("Escribe el tercer número: "))
+
+if num1 > num2:
+    if num1 > num3:
+        print("El mayor es:", num1)
+    else:
+        print("El mayor es:", num3)
+else:
+    if num2 > num3:
+        print("El mayor es", num2)
+    else:
+        print("El mayor es", num3)
