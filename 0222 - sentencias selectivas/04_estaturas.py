b12bf62f5f5deeb0fee6a69627d6d432be78b608
Miguel Bernal Marin <miguel.bernal@iteso.mx>
2021-02-22 12:36:40 -0600

    ejercicio resuelto

--- 0222 - sentencias selectivas/04_estaturas.py
+++ 0222 - sentencias selectivas/04_estaturas.py
@@ -3,3 +3,11 @@
 # considerando que menor de 1.50 metros es baja
 # y mayor a 1.70 metros es alta.
 
+estatura = float(input("Escribe tu estatura en metros: "))
+
+if estatura <= 1.50:
+    print("Tu estatura es baja")
+elif estatura >= 1.70:
+    print("Tu estatura es alta")
+else:
+    print("Tu estatura es media")
