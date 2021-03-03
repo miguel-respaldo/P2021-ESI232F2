b20615c7957832afa93c2a4cfb3ecacdfa04c3d0
Miguel Bernal Marin <miguel.bernal@iteso.mx>
2021-03-03 12:49:28 -0600

    tablas de multiplicar con while comentarios

--- 0303 - ciclos/04_tablas_con_while.py
+++ 0303 - ciclos/04_tablas_con_while.py
@@ -2,6 +2,9 @@
 tabla = int(input("¿Que tabla de multiplicar quieres?: "))
 
 i = 1
+# Mientras el valor de i es menor o igual a 10
 while i <=10:
+    # imprime la tabla de multiplicar
     print(i, "x", tabla, "=", i * tabla)
+    # e incrementa el valor de i
     i = i + 1
