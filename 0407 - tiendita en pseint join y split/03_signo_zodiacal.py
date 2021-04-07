078eaa523df79f645b6c876ba9497781757d3944
Miguel Bernal Marin <miguel.bernal@iteso.mx>
2021-04-07 12:20:26 -0500

    signo_zodiacal

---
+++ 0407 - tiendita en pseint join y split/03_signo_zodiacal.py
@@ -0,0 +1,8 @@
+# Algoritmo para determinar el signo zodiacal
+# de una persona:
+
+# 1.Utilizar la función Split para resolver
+# el problema.  Considerar  las fechas de
+# cada signo, como la presentación.
+
+fecha = input("Escribe la fecha de tu nacimeinto con formato dd/mm/aaaa: ")
