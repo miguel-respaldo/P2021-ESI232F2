2fd16fd3aac211e5eb9e1de84c0eb21cd102d2b1
Miguel Bernal Marin <miguel.bernal@iteso.mx>
2021-04-10 12:39:46 -0500

    password 2

--- tareas/password.py
+++ tareas/password.py
@@ -1,8 +1,10 @@
 # Importamos la biblioteca de Hash
 import hashlib
 # El password es: perrito
-db =["f807754c5523c1a630076b3035d484d59dc053bc95a4c9a81f2d550e252b7b0f"]
+dbu =["perro", "gato"]
+dbp =["f807754c5523c1a630076b3035d484d59dc053bc95a4c9a81f2d550e252b7b0f", "f807754c5523c1a630076b3035d484d59dc053bc95a4c9a81f2d550e252b7b0f"]
 
+usuario = input("Escribe tu usuario: ")
 password = input("Escribe tu password: ")
 cifrado = hashlib.sha3_256(password.encode())
 
@@ -11,8 +13,13 @@ cifrado = hashlib.sha3_256(password.encode())
 #print("a: ", hashlib.sha3_256("a".encode()).hexdigest())
 #print("A: ", hashlib.sha3_256("A".encode()).hexdigest())
 
-if cifrado.hexdigest() == db[0]:
-    print("Si es el password")
+
+if usuario in dbu:
+    posicion = dbu.index(usuario)
+    if cifrado.hexdigest() == dbp[posicion]:
+        print("Login existoso")
+    else:
+        print("Usuario o Password Incorrectos")
 else:
-    print("password incorrecto")
+    print("Usuario o Password Incorrectos")
 
