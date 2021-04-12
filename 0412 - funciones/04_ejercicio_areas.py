4472f88576c9c280135ae0015892e514ea395b0e
Miguel Bernal Marin <miguel.bernal@iteso.mx>
2021-04-12 11:56:12 -0500

    inicio menu 2

--- 0412 - funciones/04_ejercicio_areas.py
+++ 0412 - funciones/04_ejercicio_areas.py
@@ -1,6 +1,9 @@
 
-print("1) Calcular el área de un rectángulo")
-print("2) Calcular el área de un triángulo")
-print("3) Calcular el área de un cuadrado")
-print("4) Calcular el área de un círculo")
-print("5) Salir")
+def menu():
+    print("1) Calcular el área de un rectángulo")
+    print("2) Calcular el área de un triángulo")
+    print("3) Calcular el área de un cuadrado")
+    print("4) Calcular el área de un círculo")
+    print("5) Salir")
+
+menu()
