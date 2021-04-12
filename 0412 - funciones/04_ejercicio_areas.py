5f65302b3b27f66feea42de30a2feb0132be603e
KristoferGarcia28 <kristofer2809@gmail.com>
2021-04-12 11:58:49 -0500

    Modificación lista Kris

--- 0412 - funciones/04_ejercicio_areas.py
+++ 0412 - funciones/04_ejercicio_areas.py
@@ -1,9 +1,11 @@
 
 def menu():
+    print("selcciona una operación")
     print("1) Calcular el área de un rectángulo")
     print("2) Calcular el área de un triángulo")
     print("3) Calcular el área de un cuadrado")
     print("4) Calcular el área de un círculo")
     print("5) Salir")
-
+    resultado = int(input("Selecciona una opción"))
+    return resultado
 menu()
