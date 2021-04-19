c1b84a55f5e30ac8e14508c5e95d364da7d793d1
Miguel Bernal Marin <miguel.bernal@iteso.mx>
2021-04-19 11:40:43 -0500

    RFC fix

--- tareas/generador_de_RFC.py
+++ tareas/generador_de_RFC.py
@@ -22,6 +22,9 @@ for letra in apellido_paterno[1:]:
         break
 RFC = RFC + letra
 
+# Primera letra de Nombre
+RFC = RFC + nombre[0]
+
 # Primera letra de Apellido Materno
 RFC = RFC + apellido_materno[0]
 
