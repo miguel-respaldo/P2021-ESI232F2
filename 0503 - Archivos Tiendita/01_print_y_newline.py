b8fc550a6cb5e43b68fefdc327259e8929570071
Miguel Bernal Marin <miguel.bernal@iteso.mx>
2021-05-03 11:58:07 -0500

    newline 2

--- 0503 - Archivos Tiendita/01_print_y_newline.py
+++ 0503 - Archivos Tiendita/01_print_y_newline.py
@@ -1,8 +1,14 @@
-for i in range(5):
+for i in range(3):
     print("Hola")
 
-for i in range(5):
+for i in range(3):
     print("Hola", end=", ")
 
-for i in range(5):
+for i in range(3):
     print("Hola", end="<--\n-->")
+
+for i in range(3):
+    print("1 linea\n")
+
+for i in range(3):
+    print("2 linea\n", end="")
